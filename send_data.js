60336649c50a2c15303dd8ac677eeb2083f460ba
Arti <nalawade.arti@gmail.com>
2014-06-01 06:06:43 +0530

    Create send_data.js

---
+++ send_data.js
@@ -0,0 +1,28 @@
+/* * Contributor(s):
+ *   PATIL Kailas <patilkr24@gmail.com>
+*/
+
+// Click on Exit Link on UI
+function exitCSP(e) {
+    var mytext = " Sucessfully Exit from User CSP Add-on UI";
+    addon.port.emit("exitUserCSP", mytext);
+}
+
+// Click on Report an Issue link on UI
+function reportIssue(e) {
+    addon.port.emit("reportIssue");
+}
+
+// Function to get inferred CSP Rule array from main 
+function getInferCSPArray(webDomain) {
+    addon.port.emit("inferCSPArray", webDomain);
+}
+
+// Combine Strict feature
+function getCombineStrict(sitePolicy, userPolicy, webDomain) {
+    addon.port.emit("combineStrictPolicy", sitePolicy, userPolicy, webDomain);
+}
+
+function storeUserCSPUState(selectedDomain, userCSPUIState, flag, userCSPAll, userCSPArray) {
+    addon.port.emit("storeUserCSPUIState", selectedDomain, userCSPUIState, flag, userCSPAll, userCSPArray);
+}
